a88dc5dfb938996417ac2ebd0222ad9059b0494c
JBicera <joshuaebicera@gmail.com>
2025-04-14 23:40:51 -0700

    Update

--- student/terasort.c
+++ student/terasort.c
@@ -5,108 +5,109 @@
 #include "terarec.h"  // Provides terarec_t, teraCompare(), mpi_tera_type, etc.
 
 void terasort(terarec_t *local_data, int local_len, 
-              terarec_t **sorted_data, int* sorted_counts, long* sorted_displs) {
+              terarec_t **sortedData, int* sorted_counts, long* sorted_displs) {
     int rank, P;
     MPI_Comm_size(MPI_COMM_WORLD, &P);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    // 1. Sort own data locally
+    // 1. Sort own data locally using quicksort
     qsort(local_data, local_len, sizeof(terarec_t), teraCompare);
 
     // 2. Select P-1 samples from local sorted data
-    int nsamples = P - 1;
-    terarec_t *localSamples = malloc(nsamples * sizeof(terarec_t));
-    for (int i = 0; i < nsamples; i++) {
+    int numSamples = P - 1;
+    terarec_t *localSamples = malloc(numSamples * sizeof(terarec_t)); // Store samples
+    for (int i = 0; i < numSamples; i++) 
+    {
         int index = (i + 1) * local_len / P;
-        if (index >= local_len) index = local_len - 1;
+        if (index >= local_len) 
+            index = local_len - 1;
         localSamples[i] = local_data[index];
     }
 
     // 3. Gather samples at root
     terarec_t *globalSamples = NULL;
-    if (rank == 0) {
-        globalSamples = malloc(P * nsamples * sizeof(terarec_t));
-    }
-    MPI_Gather(localSamples, nsamples, mpi_tera_type,
-               globalSamples, nsamples, mpi_tera_type,
-               0, MPI_COMM_WORLD);
+    if (rank == 0)
+        globalSamples = malloc(P * numSamples * sizeof(terarec_t));
+    MPI_Gather(localSamples, numSamples, mpi_tera_type,
+               globalSamples, numSamples, mpi_tera_type,
+               0, MPI_COMM_WORLD); // MPI_Gather to root
     free(localSamples);
 
     // 4. Root sorts samples and selects P-1 splitters
     terarec_t *splitters = malloc((P - 1) * sizeof(terarec_t));
-    if (rank == 0) {
-        qsort(globalSamples, nsamples * P, sizeof(terarec_t), teraCompare);
-        for (int i = 1; i < P; i++) {
-            splitters[i - 1] = globalSamples[i * nsamples];
-        }
+    if (rank == 0) 
+    {
+        qsort(globalSamples, numSamples * P, sizeof(terarec_t), teraCompare);
+        for (int i = 1; i < P; i++)
+            splitters[i - 1] = globalSamples[i * numSamples]; // Pick every Pth element form samples
         free(globalSamples);
     }
 
     // 5. Broadcast splitters
-    MPI_Bcast(splitters, P - 1, mpi_tera_type, 0, MPI_COMM_WORLD);
+    MPI_Bcast(splitters, P - 1, mpi_tera_type, 0, MPI_COMM_WORLD); // Send splitters to every process
 
     // 6. Partition local data using splitters
-    int *send_counts = calloc(P, sizeof(int));  // Count of elements to send to each process
+    int *sendCount = calloc(P, sizeof(int));  // Count of elements to send to each process
 
-    // First pass: count how many items go in each bucket
-    for (int i = 0; i < local_len; i++) {
+    // Count how many items go in each bucket
+    for (int i = 0; i < local_len; i++) 
+    {
         int bucket = 0;
-        while (bucket < P - 1 && teraCompare(&local_data[i], &splitters[bucket]) > 0) {
+        while (bucket < P - 1 && teraCompare(&local_data[i], &splitters[bucket]) > 0)
             bucket++;
-        }
-        send_counts[bucket]++;
+        sendCount[bucket]++;
     }
 
     // Compute send displacements
-    int *send_displs = malloc(P * sizeof(int));
-    send_displs[0] = 0;
-    for (int i = 1; i < P; i++) {
-        send_displs[i] = send_displs[i - 1] + send_counts[i - 1];
-    }
+    int *sendDispl = malloc(P * sizeof(int));
+    sendDispl[0] = 0;
+    for (int i = 1; i < P; i++)
+        sendDispl[i] = sendDispl[i - 1] + sendCount[i - 1];
 
     // Allocate send buffer and temp position tracker
-    terarec_t *send_buffer = malloc(local_len * sizeof(terarec_t));
-    int *temp_counts = calloc(P, sizeof(int));
+    terarec_t *sendBuff = malloc(local_len * sizeof(terarec_t));
+    int *tempCounts = calloc(P, sizeof(int));
 
-    // Second pass: actually copy data to send buffer
-    for (int i = 0; i < local_len; i++) {
+    // Copy data to send buffer
+    for (int i = 0; i < local_len; i++) 
+    {
         int bucket = 0;
-        while (bucket < P - 1 && teraCompare(&local_data[i], &splitters[bucket]) > 0) {
+        while (bucket < P - 1 && teraCompare(&local_data[i], &splitters[bucket]) > 0)
             bucket++;
-        }
-        int pos = send_displs[bucket] + temp_counts[bucket]++;
-        send_buffer[pos] = local_data[i];
+        int pos = sendDispl[bucket] + tempCounts[bucket]++;
+        sendBuff[pos] = local_data[i];
     }
 
-    free(temp_counts);
+    free(tempCounts);
     free(splitters);
 
-    // 7. Exchange partitioned values
-    int *recv_counts = malloc(P * sizeof(int));
-    MPI_Alltoall(send_counts, 1, MPI_INT, recv_counts, 1, MPI_INT, MPI_COMM_WORLD);
+    // 7. Exchange partitioned data
+    int *recvCounts = malloc(P * sizeof(int));
+    MPI_Alltoall(sendCount, 1, MPI_INT, recvCounts, 1, MPI_INT, MPI_COMM_WORLD);
 
-    int *recv_displs = malloc(P * sizeof(int));
-    recv_displs[0] = 0;
-    int total_recv = recv_counts[0];
-    for (int i = 1; i < P; i++) {
-        recv_displs[i] = recv_displs[i - 1] + recv_counts[i - 1];
-        total_recv += recv_counts[i];
+    int *recvDispl = malloc(P * sizeof(int));
+    recvDispl[0] = 0;
+    int total_recv = recvCounts[0];
+    for (int i = 1; i < P; i++) 
+    {
+        recvDispl[i] = recvDispl[i - 1] + recvCounts[i - 1];
+        total_recv += recvCounts[i];
     }
 
-    terarec_t *recv_buffer = malloc(total_recv * sizeof(terarec_t));
-    MPI_Alltoallv(send_buffer, send_counts, send_displs, mpi_tera_type,
-                  recv_buffer, recv_counts, recv_displs, mpi_tera_type,
+    terarec_t *recvBuff = malloc(total_recv * sizeof(terarec_t));
+    MPI_Alltoallv(sendBuff, sendCount, sendDispl, mpi_tera_type,
+                  recvBuff, recvCounts, recvDispl, mpi_tera_type,
                   MPI_COMM_WORLD);
 
-    free(send_buffer);
-    free(send_counts);
-    free(send_displs);
-    free(recv_displs);
+    free(sendBuff);
+    free(sendCount);
+    free(sendDispl);
+    free(recvDispl);
 
     // 8. Final local sort
-    qsort(recv_buffer, total_recv, sizeof(terarec_t), teraCompare);
+    qsort(recvBuff, total_recv, sizeof(terarec_t), teraCompare);
 
-    *sorted_data = recv_buffer; // Return values
+    *sortedData = recvBuff; // Return values
 
     int my_count = total_recv;
     MPI_Allgather(&my_count, 1, MPI_INT, sorted_counts, 1, MPI_INT, MPI_COMM_WORLD);
