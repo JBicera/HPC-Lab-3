a328b178b463a7b8c685a96ee6c8b69336da6144
JBicera <joshuaebicera@gmail.com>
2025-04-18 19:01:20 -0700

    Update terasort.c

--- student/terasort.c
+++ student/terasort.c
@@ -4,44 +4,6 @@
 #include <mpi.h>
 #include "terarec.h"  // Provides terarec_t, teraCompare(), mpi_tera_type, etc.
 
-#define BYTE_AT(key, d) ((unsigned char)(key[d]))
-#define MAX_DIGIT 10  // up to 10 bytes of key for MSD radix sort
-
-static void msdRadixSortRecords(terarec_t *A, int n, int digit) {
-    if (n <= 1 || digit >= MAX_DIGIT) return;
-
-    int count[256] = {0};
-    int index[256];
-
-    for (int i = 0; i < n; i++) {
-        unsigned char d = BYTE_AT(A[i].key, digit);
-        count[d]++;
-    }
-
-    index[0] = 0;
-    for (int i = 1; i < 256; i++) {
-        index[i] = index[i - 1] + count[i - 1];
-    }
-
-    terarec_t *temp = malloc(n * sizeof(terarec_t));
-    for (int i = 0; i < n; i++) {
-        unsigned char d = BYTE_AT(A[i].key, digit);
-        temp[index[d]++] = A[i];
-    }
-    memcpy(A, temp, n * sizeof(terarec_t));
-    free(temp);
-
-    index[0] = 0;
-    for (int i = 1; i < 256; i++) {
-        index[i] = index[i - 1] + count[i - 1];
-    }
-    for (int i = 0; i < 256; i++) {
-        if (count[i] > 1) {
-            msdRadixSortRecords(A + index[i], count[i], digit + 1);
-        }
-    }
-}
-
 void terasort(terarec_t *local_data, int local_len,
               terarec_t **sortedData, int* sorted_counts, long* sorted_displs) {
     int rank, P;
@@ -49,14 +11,14 @@ void terasort(terarec_t *local_data, int local_len,
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     // Buffers reused across phases
-    int *sendCounts  = calloc(P, sizeof(int));
-    int *sendDispl   = malloc(P * sizeof(int));
-    int *recvCounts  = malloc(P * sizeof(int));
-    int *recvDispl   = malloc(P * sizeof(int));
+    int *sendCounts  = calloc(P, sizeof(int));    // counts to send
+    int *sendDispl   = malloc(P * sizeof(int));  // displacements for send
+    int *recvCounts  = malloc(P * sizeof(int));  // counts to receive
+    int *recvDispl   = malloc(P * sizeof(int));  // displacements for recv
     terarec_t *sendBuf = malloc(local_len * sizeof(terarec_t));
 
-    // 1. Sort own data locally (MSD Radix)
-    msdRadixSortRecords(local_data, local_len, 0);
+    // 1. Sort own data locally
+    qsort(local_data, local_len, sizeof(terarec_t), teraCompare);
 
     // 2. Select P-1 samples
     int numSamples = P - 1;
@@ -75,13 +37,13 @@ void terasort(terarec_t *local_data, int local_len,
                globalSamples, numSamples, mpi_tera_type,
                0, MPI_COMM_WORLD);
     free(localSamples);
-
-    // 4. Sort samples on root (MSD Radix)
+    
+    // 4. Sort samples on root
     terarec_t *splitters = malloc(numSamples * sizeof(terarec_t));
     if (rank == 0) 
     {
-        msdRadixSortRecords(globalSamples, P * numSamples, 0);
-
+        qsort(globalSamples, P * numSamples, sizeof(terarec_t), teraCompare);
+        
         // 5. Select P - 1 splitters
         for (int i = 1; i < P; i++)
             splitters[i-1] = globalSamples[i * numSamples];
@@ -138,8 +100,8 @@ void terasort(terarec_t *local_data, int local_len,
     free(sendCounts);
     free(sendDispl);
 
-    // 9. Final local sort (MSD Radix)
-    msdRadixSortRecords(recvBuf, totalRecv, 0);
+    // 9. Final local sort
+    qsort(recvBuf, totalRecv, sizeof(terarec_t), teraCompare);
     *sortedData = recvBuf;
 
     // Gather final counts and compute displs
